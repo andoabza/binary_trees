5609803be297416022ec7914313def1b05e1c02e
anda <terbu102@gmail.com>
2023-08-29 10:49:01 -0700

    10-deoth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,10 +8,9 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
 	if (tree == NULL)
 		return (0);
-	typedef size_t depth;
-	depth height;
-	height = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
-	return (height);
+	depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	return (depth);
 }
