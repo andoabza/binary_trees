e5283e814f953d88f596c3e6dea2b6b63a1b17ae
anda <terbu102@gmail.com>
2023-08-29 01:59:56 -0700

    root is

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent == NULL)
-		return (1);
-	return (0);
+	if (node == NULL)
+		return (0);
+	return (node->parent == NULL);
 }
