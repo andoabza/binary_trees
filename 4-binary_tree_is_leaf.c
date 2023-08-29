5057cebcf37ae98d01c708b7e754e953ab03f98d
anda <terbu102@gmail.com>
2023-08-29 01:30:24 -0700

    is_leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_is_leaf - a function that check if node is leaf.
  * @node: a given node to check.
+ * Return: 1 or 0 based on is leaf.
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
