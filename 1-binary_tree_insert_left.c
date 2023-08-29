d4cf7f3a84e2ec2c66e0a13b82b24f05595ab6b3
anda <terbu102@gmail.com>
2023-08-29 00:45:57 -0700

    add

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,12 +10,11 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new;
 	new = binary_tree_node(parent, value);
-
 	if (new == NULL)
 		return (NULL);
 
