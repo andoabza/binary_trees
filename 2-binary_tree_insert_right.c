baef66aef11fdb4730addb4af9a1aa478083f040
anda <terbu102@gmail.com>
2023-08-29 00:52:55 -0700

    bin right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_insert_right - a binary tree that insert to the right.
+ *
  * @parent: the root value.
  * @value: to be inserted
  *
@@ -8,25 +9,22 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        if (parent == NULL)
-                return (NULL);
-        binary_tree_t *new;
-        new = binary_tree_node(parent, value);
+	binary_tree_t *new;
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
 
 	if (new == NULL)
 		return (NULL);
-        binary_tree_t *current;
-        current = parent->right;
 
-        new->n = value;
-        new->parent = parent;
-        new->right = current;
-        new->left = NULL;
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
 
-        if (current != NULL)
-                current->parent = new;
+	parent->right = new;
 
-        parent->right = new;
-
-        return (new);
+	return (new);
 }
