dc0f7734676ce28432e19cffd046a4553f1fcd9a
anda <terbu102@gmail.com>
2023-08-29 00:26:53 -0700

    left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,29 +4,28 @@
  * @parent: the root value.
  * @value: to be inserted
  *
- * RETURN - the new node.
+ * Return: the new node.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
-                return (NULL);
+		return (NULL);
 	binary_tree_t *new;
 	new = binary_tree_node(parent, value);
-
 	if (new == NULL)
 		return (NULL);
-    	binary_tree_t *current;
-       	current = parent->left;
-	
-    	new->n = value;
-    	new->parent = parent;
-    	new->left = current;
-    	new->right = NULL;
 
-    	if (current != NULL)
+	binary_tree_t *current;
+	current = parent->left;
+
+	new->n = value;
+	new->parent = parent;
+	new->left = current;
+	new->right = NULL;
+
+	if (current != NULL)
 		current->parent = new;
 
-    	parent->left = new;
-
-    	return (new);
+	parent->left = new;
+	return (new);
 }
