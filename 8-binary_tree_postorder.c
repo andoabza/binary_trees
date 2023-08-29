14e4e642b51542b4b251727da39d5f7a0d30f2da
anda <terbu102@gmail.com>
2023-08-29 02:56:14 -0700

    post

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,7 +8,11 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+
+	}
 }
