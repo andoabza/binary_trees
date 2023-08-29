f7a17b2a18ed5448eb153d484335c4a4ebe8cf8a
anda <terbu102@gmail.com>
2023-08-29 01:12:06 -0700

    delet

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_insert_right - a binary tree that insert to the right.
  *
@@ -7,6 +8,7 @@
  *
  * RETURN - the new node.
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - a recursive function that delete anode.
+ * @tree: a node to be deleted.
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
