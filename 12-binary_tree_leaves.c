ffd5fd1edbde280a72579db4d282187cc779290b
anda <terbu102@gmail.com>
2023-08-30 09:08:00 -0700

    leave

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	left = binary_tree_leaves(tree-left);
-	right = binary_tree_leaves(tree-right);
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
 	return (left + right);
 }
